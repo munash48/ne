84328d25899efda033573acc4f005ba4bb1315da
munash48 <mugwanyan@gmail.com>
2019-01-27 14:01:47 +0300

    after setting landing page

--- onlineshopping/src/main/java/net/nasser/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/nasser/onlineshopping/controller/PageController.java
@@ -4,15 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller 
+@Controller
 public class PageController {
-	
-	@RequestMapping(value={"/","/home","/index"})
-	public ModelAndView index(){
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring mvc");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
-	
 
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
 }
